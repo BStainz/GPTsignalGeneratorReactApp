b7b84cd8b5b283ef0bbf50f844439b4a33067bfe
BStainz <brendanhannon1011@gmail.com>
2023-06-30 23:24:42 -0400

    initial commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,9 +1,40 @@
 import { Configuration, OpenAIApi } from "openai";
+import axios from 'axios';
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+
+
+async function getWeeklyStockData(ticker) {
+  const apiKey = process.env.ALPHA_VANTAGE_API_KEY;
+  const functionParam = 'TIME_SERIES_WEEKLY';
+  const datatype = 'json';
+  const url = `https://www.alphavantage.co/query?function=${functionParam}&symbol=${ticker}&datatype=${datatype}&apikey=${apiKey}`;
+
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    if (data.Note) {
+      throw new Error(data.Note);
+    }
+
+    const mostRecentDataPoint = Object.keys(data['Weekly Time Series'])[0];
+    const mostRecentData = data['Weekly Time Series'][mostRecentDataPoint];
+
+    const open = mostRecentData['1. open'];
+    const high = mostRecentData['2. high'];
+    const low = mostRecentData['3. low'];
+    const close = mostRecentData['4. close'];
+    const volume = mostRecentData['5. volume'];
+
+    return { ticker, open, high, low, close, volume };
+
+  } catch (error) {
+    throw error;
+  }
+}
 
 export default async function (req, res) {
   if (!configuration.apiKey) {
@@ -15,25 +46,26 @@ export default async function (req, res) {
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const ticker = req.body.ticker || '';
+  if (ticker.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "Please enter a valid stock symbol",
       }
     });
     return;
   }
 
   try {
+    const stockData = await getWeeklyStockData(ticker); 
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
+      prompt: generatePrompt(stockData),
       temperature: 0.6,
+      max_tokens: 200,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
-    // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
       res.status(error.response.status).json(error.response.data);
@@ -48,15 +80,7 @@ export default async function (req, res) {
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+function generatePrompt(stockData) {
+  const { ticker, open, high, low, close, volume } = stockData;
+  return `Given the historical weekly trend, the current price of ${close}, volume of ${volume}, opening price of ${open}, highest price of ${high}, and lowest price of ${low} for ${ticker}, analyze the price movement and volatility, and provide a detailed trading signal with speculative buy, sell, and stop loss targets.`;
 }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [tickerInput, setTickerInput] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
@@ -14,7 +14,7 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ ticker: tickerInput }),
       });
 
       const data = await response.json();
@@ -23,9 +23,8 @@ export default function Home() {
       }
 
       setResult(data.result);
-      setAnimalInput("");
+      setTickerInput("");
     } catch(error) {
-      // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
     }
@@ -35,21 +34,21 @@ export default function Home() {
     <div>
       <Head>
         <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <link rel="icon" href="/stock.png" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <img src="/stock2.png" className={styles.icon} />
+        <h3>Trading Signal Generator</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="ticker"
+            placeholder="Enter a stock symbol"
+            value={tickerInput}
+            onChange={(e) => setTickerInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Generate Signal" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
